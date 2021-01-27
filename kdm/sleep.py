2abd0d9133168f1859a0649f2184c2dd47199d87
Kenneth D. Mankoff <mankoff@gmail.com>
2021-01-27 08:28:32 -0800

    Sleep on system sleep; Sleep cron even if silent start

--- kdm/sleep.py
+++ kdm/sleep.py
@@ -1,4 +1,4 @@
-from talon import actions, cron, speech_system
+from talon import ui, actions, cron, speech_system, app
 
 job = None
 def on_phrase(j):
@@ -8,3 +8,16 @@ def on_phrase(j):
         job = cron.after('15m', actions.speech.disable)
 
 speech_system.register('post:phrase', on_phrase)
+
+def on_ready():
+    global job
+    if actions.speech.enabled():
+       job = cron.after('15m', actions.speech.disable)
+       
+app.register("ready", on_ready)
+
+
+# https://github.com/nriley/knausj_talon/blob/nriley/misc/screen.py
+
+ui.register('screen_sleep', lambda e: actions.speech.disable())
+# ui.register('screen_wake')
