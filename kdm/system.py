42f8b671c4e6647bf18e3f7a449d86f5716a2a92
Kenneth D. Mankoff <mankoff@gmail.com>
2021-01-27 08:28:20 -0800

    Simpler desk switching

--- kdm/system.py
+++ kdm/system.py
@@ -1,13 +1,13 @@
-from talon import ctrl, Module, Context
-import os
+# from talon import ctrl, Module, Context
+# import os
 
-ctx = Context()
-mod = Module()
+# ctx = Context()
+# mod = Module()
 
-@mod.action_class
-class Actions:
-    def switch_desk(index: str):
-        """Switch to a virtual desktop by index (starting at 1)"""
-        os.system("wmctrl -s " + str(int(index) - 1))
-        # OR: os.system("xdotool set_desktop " + str(int(index) - 1))
+# @mod.action_class
+# class Actions:
+#     def switch_desk(index: str):
+#         """Switch to a virtual desktop by index (starting at 1)"""
+#         os.system("wmctrl -s " + str(int(index) - 1))
+#         # OR: os.system("xdotool set_desktop " + str(int(index) - 1))
         
